7b495b93c61ea96d39efc98a24abe58b0c192004
Renate Schwank <schwank@cip.ifi.lmu.de>
2022-12-02 12:55:31 +0100

    fixing typos, adding libraries (string.h (for memset) and netdb.h (for getaddrinfo)), correcting order of methods getSocketDescriptorAndConnect - main, removing & at address.sin_addr statement

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <getopt.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 
 #define GAMEKINDNAME "NMMorris"
@@ -14,6 +16,66 @@
 void printAnweisung(){
     printf("-g <GAME-ID> 13-stellige Game-ID\n");
     printf("-p gewünschte Spielernummer\n");
+}
+
+/*getSocketDescriptorAndConnect creates a socket and tries to connect to the LMU server.
+* returns socket file descriptor socketfd upon success, otherwise -1
+*/
+int getSocketDescriptorAndConnect(){
+    int socketfd;
+
+    struct sockaddr_in address;
+    memset(&address, 0, sizeof(address)); //Filling struct values with 0
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORTNUMBER);
+
+    //Retrieving the server address using getaddrinfo()
+    struct addrinfo hints, *res, *result;
+
+    memset (&hints, 0, sizeof (hints));
+    hints.ai_family = AF_INET; //using IPv4 addresses
+    hints.ai_socktype = SOCK_STREAM; //using TCP connection
+    hints.ai_protocol = IPPROTO_TCP; //using TCP protocol
+
+    if(getaddrinfo(HOSTNAME, NULL, &hints, &result) != 0) {
+        perror("getaddrinfo"); //TODO Error handling
+        return -1;
+    }
+
+    res = result;
+
+    /*Iterating through the list of address structures obtained by getaddrinfo().
+    * Trying to connect to one of the sockets.
+    * If socket creation fails, we immediately continue with next iteration (continue).
+    * If connection can be established, loop is ended (break).
+    * Otherwise, socket gets closed and loop is continued.
+    */
+    while (res) {
+        res = res->ai_next;
+
+        if((socketfd = socket(AF_INET, SOCK_STREAM, 0))==-1) {
+            continue;
+        }
+
+        address.sin_addr = ((struct sockaddr_in *) res->ai_addr)->sin_addr;
+
+        if(connect(socketfd, (struct sockaddr *) &address, sizeof(address)) != -1) {
+            break;
+        }
+
+        close(socketfd);
+    }
+    
+    freeaddrinfo(result); //no longer needed since IP-address of server has been resolved
+
+    //All connection attempts failed, i.e. res reached the end of the list of address structures
+    if(NULL == res) {
+        printf("Could not connect to server %s.\n", HOSTNAME);
+        //TODO Error handling
+        return -1;
+    }
+
+    return socketfd;
 }
 
 
@@ -63,67 +125,8 @@ int main(int argc,char**argv){
     int socketfd = getSocketDescriptorAndConnect();
     //TODO error handling for socketfd == -1
 
-    performConnection(socketfd, game_id);
+    //performConnection(socketfd, game_id);
 
     return 0;
 }
 
-/*getSocketDescriptorAndConnect creates a socket and tries to connect to the LMU server.
-* returns socket file descriptor socketfd upon success, otherwise -1
-*/
-int getSocketDescriptorAndConnect(){
-    int socketfd;
-
-    struct sockaddr_in address;
-    memset(&addr, 0, sizeof(addr)); //Filling struct values with 0
-    address.sin_family = AF_INET;
-    address.sin_port = htons(PORTNUMBER);
-
-    //Retrieving the server address using getaddrinfo()
-    struct addrinfo hints, *res, *result;
-
-    memset (&hints, 0, sizeof (hints));
-    hints.ai_family = AF_INET; //using IPv4 addresses
-    hints.ai_socktype = SOCK_STREAM; //using TCP connection
-    hints.ai_protocol = IPPROTO_TCP; //using TCP protocol
-
-    if(getaddrinfo(HOSTNAME, NULL, &hints, &result) != 0) {
-        perror("getaddrinfo"); //TODO Error handling
-        return -1;
-    }
-
-    res = result;
-
-    /*Iterating through the list of address structures obtained by getaddrinfo().
-    * Trying to connect to one of the sockets.
-    * If socket creation fails, we immediately continue with next iteration (continue).
-    * If connection can be established, loop is ended (break).
-    * Otherwise, socket gets closed and loop is continued.
-    */
-    while (res) {
-        res = res->ai_next;
-
-        if((socketfd = socket(AF_INET, SOCK_STREAM, 0))==-1) {
-            continue;
-        }
-
-        address.sin_addr = &((struct sockaddr_in *) res->ai_addr)->sin_addr;
-
-        if(connect(socketfd, (struct sockaddr *) &address, sizeof(address)) != -1) {
-            break;
-        }
-
-        close(socketfd);
-    }
-    
-    freeaddrinfo(result); //no longer needed since IP-address of server has been resolved
-
-    //All connection attempts failed, i.e. res reached the end of the list of address structures
-    if(NULL == res) {
-        printf("Could not connect to server %s.\n", HOSTNAME);
-        //TODO Error handling
-        retrun -1;
-    }
-
-    return socketfd;
-}

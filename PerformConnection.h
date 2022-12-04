54f009ff7ad222192272b9f9c2c691e8b8bca634
Mahmut Cinar <M.Cinar@campus.lmu.de>
2022-12-04 16:15:58 +0100

    final tweaks to make the performConnection() runnable

--- PerformConnection.h
+++
@@ -1,6 +0,0 @@
-#ifndef PerformConnection
-#define PerformConnection
-
-void getServermsg(int fileDescriptor);
-void sendMsgToServer(int fileDescriptor, char* msgInput);
-int performConnection (int fileDescrptor, char* gameID);

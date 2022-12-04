84d695b6d32669e34e2122ef408582b7d618c48b
Charlotte Ren <charlotte.ren@campus.lmu.de>
2022-12-04 16:41:26 +0100

    marked TODO in main.c

--- main.c
+++ main.c
@@ -109,6 +109,7 @@ int main(int argc,char**argv){
             return 0;
         }
     }
+    // TODO: define and process option for configuration file name 
 
     LoadConfigParam(&config,configFile);
     DumpConfig(&config);

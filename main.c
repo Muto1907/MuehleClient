2fab0f03f425b5da9913cc8cd1729bbad7699d35
Charlotte Ningxin Ren <renc@aisch.cip.ifi.lmu.de>
2022-12-09 12:46:19 +0100

    deleted TODO

--- main.c
+++ main.c
@@ -113,7 +113,6 @@ int main(int argc,char**argv){
             return 0;
         }
     }
-    // TODO: define and process option for configuration file name 
 
     LoadConfigParam(&config,configFile);
     DumpConfig(&config);

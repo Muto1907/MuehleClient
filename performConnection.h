9842ed9d4eefca715d29015ceec2f9a023589b1f
Renate Schwank <schwank@cip.ifi.lmu.de>
2023-01-07 22:01:10 +0100

    fix incompatible function signatures in performConnection.c and .h respectively

--- performConnection.h
+++ performConnection.h
@@ -10,7 +10,7 @@ extern char serverMsg[];
 
 void getServermsg(int fileDescriptor);
 void sendMsgToServer(int fileDescriptor, char* msgInput);
-int performConnection (int fileDescriptor, char* gameID, PARAM_CONFIG_T* cfg);
+int performConnection (int fileDescriptor, char* gameID, PARAM_CONFIG_T* cfg, int *initial_shm);
 char** serverMsgToLines(char* serverMsg, char** linesOfServermsg);
 
 GAMEINFO* setParam();
